15efaa5af10cbb57927131abe2d4c9dd4b206801
Gius <giusmile67@gmail.com>
2017-11-05 17:45:51 +0100

    exercices

---
+++ js/app.js
@@ -0,0 +1,41 @@
+(function(){
+//Afficher le tableau d'objet dans le html
+	let classicMovies=[
+					{
+					titre:"King Kong",
+					date:1933,
+					realisateur:"Merian C. Cooper, Ernest B. Schoedsack",
+					affiche:"image/king-kong.jpg",
+					genre: "Horreur, fantastique",
+					sortieDVD : "22 novembre 2005"
+
+					},
+					{
+					titre:"Scarface",
+					date:1932,
+					realisateur:"Howard Hawks",
+					affiche:"image/camonte.jpg",
+					genre: "Policier, action",
+					sortieDVD : "23 décembre 2005"
+
+					},
+					{
+					titre:"Autant en emporte le vent",
+					date:1939,
+					realisateur:"Victor Fleming",
+					affiche:"image/autant-emporte-le-vent.jpg",
+					genre: "Drammatique, guerre",
+					sortieDVD : "19 mars 2005"
+
+					}
+
+
+
+
+
+	];
+
+	console.log(classicMovies);
+
+
+})();
